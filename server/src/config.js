76ad2cafb8f163d1002ecdcaaf83d56a88386bf9
Rutuja_22 <149168355+rutxyz@users.noreply.github.com>
2024-06-12 22:40:51 +0530

    Update config.js

--- server/src/config.js
+++ server/src/config.js
@@ -1,6 +1,6 @@
 // server/src/config.js
 module.exports = {
-    MONGODB_URI: process.env.MONGO_URI || 'mongodb+srv://rutuja03:vHGb5G2Y8lT5CNbS@cluster0.yvs1pu5.mongodb.net/crm',
+    MONGODB_URI: process.env.MONGO_URI || 'mongodb+srv://<user>:<password>@cluster0.yvs1pu5.mongodb.net/crm',
     PORT: process.env.PORT || 5001,
   };
   
